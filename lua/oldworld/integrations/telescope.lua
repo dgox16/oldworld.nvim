fa7ca3315611bd0f45bd4fbf30ac999dfbfb4487
Diego <dgox16@gmail.com>
2024-05-15 00:04:06 -0600

    Tweaks in telescope

--- lua/oldworld/integrations/telescope.lua
+++ lua/oldworld/integrations/telescope.lua
@@ -3,11 +3,11 @@ local p = require("oldworld.palette")
 return {
     TelescopeBorder = { fg = p.bg, bg = p.bg },
     TelescopeNormal = { fg = p.fg, bg = p.bg },
-    TelescopePreviewTitle = { fg = p.black, bg = p.cyan, bold = true },
-    TelescopeResultsTitle = { fg = p.bg, bg = p.bg },
-    TelescopePromptTitle = { fg = p.black, bg = p.red, bold = true },
-    TelescopePromptBorder = { fg = p.gray1, bg = p.gray1 },
-    TelescopePromptNormal = { fg = p.fg, bg = p.gray1 },
+    TelescopePreviewTitle = { fg = p.bright_cyan, bg = p.gray3, italic = true },
+    TelescopeResultsTitle = { fg = p.bright_red, bg = p.gray3, italic = true },
+    TelescopePromptTitle = { fg = p.fg, bg = p.gray3, italic = true },
+    TelescopePromptBorder = { fg = p.gray3, bg = p.bg },
+    TelescopePromptNormal = { fg = p.fg, bg = p.bg },
     TelescopePromptCounter = { fg = p.gray4, bg = p.gray1 },
-    TelescopeMatching = { fg = p.yellow, underline = true },
+    TelescopeMatching = { fg = p.yellow, bold = true },
 }
