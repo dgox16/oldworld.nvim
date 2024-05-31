3e657d5457c662f9ade47796f7adc0a1b8ccc721
Diego <dgox16@gmail.com>
2024-05-30 21:46:02 -0600

    Flash.nvim with better contrast

--- lua/oldworld/groups/integrations/flash.lua
+++ lua/oldworld/groups/integrations/flash.lua
@@ -2,7 +2,8 @@ local p = require("oldworld.palette")
 
 return {
     FlashLabel = { fg = p.bright_green, bg = p.bg, bold = true },
-    FlashBackdrop = { fg = p.gray4 },
+    FlashBackdrop = { fg = p.subtext4 },
     FlashMatch = { fg = p.bright_purple, bg = p.bg },
     FlashCurrent = { fg = p.bright_cyan, bg = p.bg },
+    FlashPrompt = { bg = p.bg_dark },
 }
