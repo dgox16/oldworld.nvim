c196f070f06ce2213617db9fa8231b24a4becbe6
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-07-20 18:12:21 -0600

    fix: fg changed to bg

--- lua/oldworld/groups/integrations/illuminate.lua
+++ lua/oldworld/groups/integrations/illuminate.lua
@@ -1,7 +1,7 @@
 local p = require("oldworld.palette")
 
 return {
-	IlluminatedWordText = { fg = p.gray3 },
-	IlluminatedWordRead = { fg = p.gray3 },
-	IlluminatedWordWrite = { fg = p.gray3 },
+	IlluminatedWordText = { bg = p.gray3 },
+	IlluminatedWordRead = { bg = p.gray3 },
+	IlluminatedWordWrite = { bg = p.gray3 },
 }
