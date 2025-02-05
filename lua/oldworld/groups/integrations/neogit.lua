3356dbbe2e8f47e0e0e4a61223388c1d5d0418eb
Diego Armando <dgox16@gmail.com>
2025-02-04 22:25:45 -0600

    feat(neogit): Neogit integration closer to the design guidelines.

--- lua/oldworld/groups/integrations/neogit.lua
+++ lua/oldworld/groups/integrations/neogit.lua
@@ -1,14 +1,14 @@
 local palette = require("oldworld.palette")
 
 return {
-	NeogitDiffDeleteHighlight = { bg = palette.dark_orange, fg = palette.brightest_orange },
-	NeogitDiffDelete = { bg = palette.dark_orange, fg = palette.orange },
-	NeogitDiffDeleteCursor = { bg = palette.brightest_orange, fg = palette.dark_orange },
+    NeogitDiffDeleteHighlight = { bg = palette.dark_orange, fg = palette.bright_orange },
+    NeogitDiffDelete = { bg = palette.dark_orange, fg = palette.orange },
+    NeogitDiffDeleteCursor = { bg = palette.bright_orange, fg = palette.dark_orange },
 
-	NeogitDiffAddHighlight = { bg = palette.dark_green, fg = palette.brightest_green },
-	NeogitDiffAdd = { bg = palette.dark_green, fg = palette.green },
-	NeogitDiffAddCursor = { bg = palette.brightest_green, fg = palette.dark_green },
+    NeogitDiffAddHighlight = { bg = palette.dark_green, fg = palette.bright_green },
+    NeogitDiffAdd = { bg = palette.dark_green, fg = palette.green },
+    NeogitDiffAddCursor = { bg = palette.bright_green, fg = palette.dark_green },
 
-	NeogitDiffContextHighlight = { bg = palette.gray2 },
-	NeogitDiffContext = { bg = palette.bg },
+    NeogitDiffContextHighlight = { bg = palette.gray2 },
+    NeogitDiffContext = { bg = palette.bg },
 }
